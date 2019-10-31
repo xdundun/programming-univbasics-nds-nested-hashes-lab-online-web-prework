2f5eef2afa592031e0151e92c6c668955f448cec
Dardan Gjonbalaj <xdundun@gmail.com>
2019-10-31 20:52:49 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -29,4 +29,5 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  puts aoh[row][key] = new_value
 end
