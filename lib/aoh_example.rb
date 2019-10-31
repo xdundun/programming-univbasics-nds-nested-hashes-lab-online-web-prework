0ceeac8d75037daf3dc92004f4ee4f5240ea15f0
Dardan Gjonbalaj <xdundun@gmail.com>
2019-10-31 20:54:46 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -9,17 +9,26 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
+  names = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  #puts names
+  return names
 end
 
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  names = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  #puts names
+  return names
 end
 
 def aoh_lookup(aoh, row, key)
+    return aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+   aoh[row][key] = new_value
+    return aoh
 end
